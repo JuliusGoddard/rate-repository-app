cd1ad25b01fed255b9ac7d7f5c93d3eb226255e0
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-11-12 14:12:24 +0000

    first commit

---
+++ setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:19000',
+      changeOrigin: true,
+    })
+  );
+};
